ec68f787534878a42962fb658f7c4ff483120d90
zeviel <77536370+zeviel@users.noreply.github.com>
2022-08-27 01:13:11 +0800

    Create apis_guru.py

---
+++ src/apis_guru.py
@@ -0,0 +1,19 @@
+from requests import get
+
+
+class APIsGuru:
+	def __init__(self):
+		self.api = "https://api.apis.guru"
+		self.headers = {
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
+		}
+
+	def get_all_apis(self):
+		return geet(
+			f"{self.api}/list.json",
+			headers=self.headers).json()
+
+	def get_basic_metrics(self):
+		return get(
+			f"{self.api}/metrics.json",
+			headers=self.headers).json()
