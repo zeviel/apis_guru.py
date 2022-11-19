8a6c569bbafdb39064e74ab444171f42139919dc
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:47:57 +0800

    Update apis_guru.py

--- src/apis_guru.py
+++ src/apis_guru.py
@@ -1,19 +1,18 @@
 from requests import get
 
-
 class APIsGuru:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://api.apis.guru"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
 		}
 
-	def get_all_apis(self):
+	def get_all_apis(self) -> dict:
 		return geet(
 			f"{self.api}/list.json",
 			headers=self.headers).json()
 
-	def get_basic_metrics(self):
+	def get_basic_metrics(self) -> dict:
 		return get(
 			f"{self.api}/metrics.json",
 			headers=self.headers).json()
